aa1d2f10f784267623d43f0f9ffc88e09ff3fea4
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-23 16:51:16 +0100

    urls working

--- listings/routes/main.py
+++ listings/routes/main.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, redirect, url_for
 from ..database.db import db
 from ..models.listing import Listing, Url_input
+import webbrowser
 
 
 main_routes = Blueprint("main", __name__)
@@ -14,7 +15,7 @@ def index():
         return render_template("index.html", listings=listings)
     else:
         url = request.form["url"]
-        shorten_url = 'potato'
+        shorten_url = 'mouse'
         new_url = Url_input(url=url, shorten_url=  shorten_url )
         db.session.add(new_url)
         db.session.commit()
@@ -33,7 +34,10 @@ def index():
 @main_routes.route('/<path:path>')
 def catch_all(path):
     print(path)
-    found_url = Url_input.get_main_url(shorten_url = path)
+    found_url =  Url_input.query.filter_by(shorten_url=path).first()
     print('catchall',found_url)
-    return 'You want path: %s' % found_url
+    long_url = found_url.url
+    print('longerurl',long_url)
+    # return 'You want path: %s' % long_url
+    return webbrowser.open_new_tab(f"http://www.{long_url}")
 
